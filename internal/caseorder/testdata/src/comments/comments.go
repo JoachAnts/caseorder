f1a6155222ffce2979349e5ca480fb63bb98830a
Joe Chance <joe.chance@hotmail.com>
2026-04-04 10:29:05 +1100

    Test for another comment edge case

--- internal/caseorder/testdata/src/comments/comments.go
+++ internal/caseorder/testdata/src/comments/comments.go
@@ -8,10 +8,18 @@ func comments(x int) {
 		// Comment for 2
 		println(2)
 		// End comment for 2
+	case 5:
+		return
+	/*
+	 * Multi line indented comment for 3
+	 * This is another line
+	 */
+	case 3: // want "case 3 should come before 5"
+		println(1)
 	/**
 	Multi line comment for 1
 	*/
-	case 1: // want "case 1 should come before 2"
+	case 1: // want "case 1 should come before 3"
 		/* Comment for 1 */
 		println(1)
 		/* End comment for 1 */
