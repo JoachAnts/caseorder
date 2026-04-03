a50ffcf436472c0aa2346921ad62df97f38d3084
Joe Chance <joe.chance@hotmail.com>
2026-04-04 09:55:19 +1100

    Update test case

--- internal/caseorder/testdata/src/comments/comments.go
+++ internal/caseorder/testdata/src/comments/comments.go
@@ -7,11 +7,14 @@ func comments(x int) {
 	case 2:
 		// Comment for 2
 		println(2)
+		// End comment for 2
 	/**
 	Multi line comment for 1
 	*/
 	case 1: // want "case 1 should come before 2"
 		/* Comment for 1 */
 		println(1)
+		/* End comment for 1 */
+		// This should stay here
 	}
 }
